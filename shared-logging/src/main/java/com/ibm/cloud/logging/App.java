8860b2af6075f7b637219985144a039f91ff9ba7
Billy Korando <William.Korando@ibm.com>
2018-10-09 14:14:23 -0500

    Adding starters

---
+++ shared-logging/src/main/java/com/ibm/cloud/logging/App.java
@@ -0,0 +1,13 @@
+package com.ibm.cloud.logging;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
